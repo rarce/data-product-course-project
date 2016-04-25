08930631612f5b88834ae117538d1a970fcd9dbf
Roberto Arce <robertoarcee@gmail.com>
2016-04-25 03:35:20 -0300

    add slidify presentation

--- data/eda.R
+++ data/eda.R
@@ -1,5 +1,4 @@
 train <- read.csv("data/train.csv")
-
 # processing data
 train$Survived <- factor(train$Survived, levels=c(1,0))
 levels(train$Survived) <- c("Survived", "Died")
@@ -7,5 +6,4 @@ train$Pclass <- as.factor(train$Pclass)
 levels(train$Pclass) <- c("1st Class", "2nd Class", "3rd Class")
 train$Gender <- factor(train$Sex, levels=c("female", "male"))
 levels(train$Gender) <- c("Female", "Male")
-
 write.csv(train, file = "shiny-app/titanic.csv")
